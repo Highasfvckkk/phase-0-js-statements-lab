f92212dfcb259d42e2987797dada57c6ab9ba403
Kinoti <vickykinoti001@gmail.com>
2025-12-07 10:02:39 +0300

    Final Commit

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,18 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+  console.log(`Watering on`);
+}
+console.log(`Watering off`);
+
+if (timeOfDay !== `morning`) {
+  console.log(`Lights on`);
+}
+console.log(`Lights off`);
+
+while (soilMoisture <= 40) {
+  console.log(soilMoisture);
+  soilMoisture = soilMoisture + 5;
+}
